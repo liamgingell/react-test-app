18341741fa914f64f0d3f34d66a1ba518c5d8b7c
Liam Gingell <elgee95@gmail.com>
2025-04-27 13:54:21 +0900

    Learned to edit html into JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ import './App.css'
 function App() {
 
   return (
-      <div className="container">
-        <svg>
-          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-        </svg>
-      </div>
+    <div>
+    <h1>Test title</h1>
+    <svg>
+      <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+    </svg>
+    <form>
+      <input type="text" />
+    </form>
+  </div>
+
   )
 }
 
