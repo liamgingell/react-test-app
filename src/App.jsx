845f718d1bcfd8282955141f94e137de24a94f3e
Liam Gingell <elgee95@gmail.com>
2025-04-27 15:04:42 +0900

    Made separate components for the list and listing items

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
-  const animalsList = animals.map((animal) => {
-    return <li key={animal}>{animal}</li>
-  })
-
-return (
-  <div>
-    <h1>Animals: </h1>
-      <ul>
-        {animalsList}
-      </ul>
-  </div>
-  )
+function ListItem(props) {
+  return <li>{props.animal}</li>
 }
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
+}
+
+function App() {
+  const animals = ["Lion", "Cow", "Snake", "Lizard"]
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+        <List animals={animals} />
+    </div>
+    )
+  }
 
 export default App
