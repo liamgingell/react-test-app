244c08e1fd534d8aa5d660c808c9e42169753b7c
Liam Gingell <elgee95@gmail.com>
2025-04-27 15:40:00 +0900

    Started making it as a ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,37 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
-function ListItem(props) {
-  return <li>{props.animal}</li>
-}
+// function ListItem(props) {
+//   return <li>{props.animal}</li>
+// }
 
 function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+  return <div>There are no animals on the list!</div>;
+  }
+
   return (
     <ul>
       {props.animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />;
+        return <li key={animal}>{animal}</li>
       })}
     </ul>
   );
 }
 
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"]
+  const animals = []
 
   return (
     <div>
       <h1>Animals: </h1>
         <List animals={animals} />
     </div>
-    )
+    );
   }
 
 export default App
